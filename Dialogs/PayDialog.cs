6f384ab6e9bb2fd1be41a14b44c4b5740e0386ae
hectordiazmtz <30422078+hectordiazmtz@users.noreply.github.com>
2017-08-02 10:48:19 -0700

    Added files via upload
    Original Split It code

---
+++ Dialogs/PayDialog.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Connector;
+
+namespace LuisBot.Dialogs
+{
+    using Microsoft.Bot.Builder.Dialogs;
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.Bot.Connector;
+
+    [Serializable]
+    public class PayDialog : IDialog<int>
+    {
+        private string name;
+        private int attempts = 3;
+
+        public PayDialog(string name)
+        {
+            this.name = name;
+        }
+
+        public async Task StartAsync(IDialogContext context)
+        {
+            await context.SayAsync($"How much did { this.name } pay?");
+
+            context.Wait(this.MessageReceivedAsync);
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
+        {
+            var message = await result;
+
+            int pay;
+
+            if (Int32.TryParse(message.Text, out pay) && (pay > 0))
+            {
+                context.Done(pay);
+            }
+            else
+            {
+                --attempts;
+                if (attempts > 0)
+                {
+                    await context.SayAsync("I'm sorry, I don't understand your reply. How much did you pay (e.g. '100')?");
+
+                    context.Wait(this.MessageReceivedAsync);
+                }
+                else
+                {
+                    context.Fail(new TooManyAttemptsException("Message was not a valid quantity."));
+                }
+            }
+        }
+    }
+}
---
+++ Dialogs/RootLuisDialog.cs
@@ -0,0 +1,118 @@
+﻿namespace LuisBot.Dialogs
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Web;
+    using Microsoft.Bot.Builder.Dialogs;
+    using Microsoft.Bot.Builder.FormFlow;
+    using Microsoft.Bot.Builder.Luis;
+    using Microsoft.Bot.Builder.Luis.Models;
+    using Microsoft.Bot.Connector;
+
+    [LuisModel("1a8f1b9d-2326-4df5-b346-098ebd29ebbd", "c99516f757a94f6882b5485bba38e524", domain: "westus.api.cognitive.microsoft.com", staging: false)]
+    [Serializable]
+    public class RootLuisDialog : LuisDialog<object>
+    {
+        private const string EntityMoney = "builtin.money";
+        private const string EntityNumber = "builtin.number";
+        private const string EntityFriend = "Friend";
+
+        private Dictionary<string, Double> amountPaid = new Dictionary<string, Double>();
+        private Dictionary<string, int> participants = new Dictionary<string, int>();
+
+        private Double billTotal = 0.0;
+        private int numberShares = 0;
+
+        [LuisIntent("")]
+        [LuisIntent("None")]
+        public async Task None(IDialogContext context, LuisResult result)
+        {
+            string message = $"Sorry, I did not understand '{result.Query}'. Type 'help' if you need assistance.";
+
+            await context.SayAsync(message, message);
+
+            context.Wait(this.MessageReceived);
+        }
+
+        [LuisIntent("SplitBill")]
+        public async Task SplitBill(IDialogContext context, IAwaitable<IMessageActivity> activity, LuisResult result)
+        {
+            var message = await activity;
+            await context.SayAsync($"Welcome to Split It! We are analyzing your message: '{message.Text}'...", $"Welcome to Split It! We are analyzing your message: '{message.Text}'...");
+            await context.SayAsync($"Who paid the bill and what was the amount?", $"Who paid the bill and what was the amount?");
+            context.Wait(this.MessageReceived);
+        }
+
+        [LuisIntent("AddParticipant")]
+        public async Task AddParticipant(IDialogContext context, LuisResult result)
+        {
+            EntityRecommendation friendEntityRecommendation;
+            EntityRecommendation numberEntityRecommendation;
+
+            if (result.TryFindEntity(EntityFriend, out friendEntityRecommendation))
+            {
+                if (result.TryFindEntity(EntityNumber, out numberEntityRecommendation))
+                {
+                    participants.Add(friendEntityRecommendation.Entity, Int32.Parse(numberEntityRecommendation.Entity));
+                    numberShares += Int32.Parse(numberEntityRecommendation.Entity);
+                    await context.SayAsync($"You said '{friendEntityRecommendation.Entity}' with number '{numberEntityRecommendation.Entity}'", $"You said '{friendEntityRecommendation.Entity}' with number '{numberEntityRecommendation.Entity}'");
+                }
+                else
+                {
+                    participants.Add(friendEntityRecommendation.Entity, 1);
+                    numberShares++;
+                    await context.SayAsync($"You said '{friendEntityRecommendation.Entity}'", $"You said '{friendEntityRecommendation.Entity}'");
+                }
+            }
+
+            await context.SayAsync($"Do you want to add more participants or shall I finalize the expense?", $"Do you want to add more participants or shall I finalize the expense?");
+            context.Wait(this.MessageReceived);
+        }
+
+        [LuisIntent("AddSpender")]
+        public async Task AddSpender(IDialogContext context, LuisResult result)
+        {
+            EntityRecommendation friendEntityRecommendation;
+            EntityRecommendation moneyEntityRecommendation;
+
+            if (result.TryFindEntity(EntityFriend, out friendEntityRecommendation))
+            {
+                if (result.TryFindEntity(EntityNumber, out moneyEntityRecommendation))
+                {
+                    amountPaid.Add(friendEntityRecommendation.Entity, Double.Parse(moneyEntityRecommendation.Entity));
+                    billTotal += Double.Parse(moneyEntityRecommendation.Entity);
+
+                    participants.Add(friendEntityRecommendation.Entity, 1);
+                    numberShares++;
+                    await context.SayAsync($"You added '{friendEntityRecommendation.Entity}' with amount '{moneyEntityRecommendation.Entity}'", $"You added '{friendEntityRecommendation.Entity}' with amount '{moneyEntityRecommendation.Entity}'");
+                }
+            }
+
+            await context.SayAsync($"Did anyone else contribute? If so, how much did he or she pay?", $"Did anyone else contribute? If so, how much did he or she pay?");
+            context.Wait(this.MessageReceived);
+        }
+
+        [LuisIntent("EndList")]
+        public async Task EndList(IDialogContext context, LuisResult result)
+        {
+            await context.SayAsync($"Do you want to add someone to the expense?", $"Do you want to add someone to the expense?");
+            context.Wait(this.MessageReceived);
+        }
+
+        [LuisIntent("CalculateBill")]
+        public async Task CalculateBill(IDialogContext context, LuisResult result)
+        {
+            await context.SayAsync($"Finalizing expenses", $"Finalizing expenses");
+
+            // Calculate how much is owed per person
+            Double amtPerShare = Math.Round(billTotal / numberShares, 2);
+
+            await context.SayAsync($"Everyone owes '{amtPerShare}'", $"Everyone owes '{amtPerShare}'");
+
+            context.Done<object>(null);
+        }
+
+    }
+}
